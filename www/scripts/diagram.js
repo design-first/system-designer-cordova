e15041c99094a6c65800ac46a9b43e35481dd350
Erwan Carriou <erwan.carriou@mac.com>
2019-12-30 00:42:26 +0100

    Bump 4.4.0

--- www/scripts/diagram.js
+++ www/scripts/diagram.js
@@ -1,3 +1,3 @@
-// System Designer - Copyright 2019 Erwan Carriou
+// System Designer - Copyright 2020 Erwan Carriou
 // Licensed under the Apache License, Version 2.0 (the "License")
 runtime.install();
