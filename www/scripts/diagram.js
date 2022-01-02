6c56ad87ada9343d410d322d9362b0018955f931
Erwan Carriou <erwan.carriou@mac.com>
2022-01-03 00:58:18 +0100

    chore: bump 5.1.0

--- www/scripts/diagram.js
+++ www/scripts/diagram.js
@@ -1,3 +1,3 @@
-// System Designer - Copyright 2021 Erwan Carriou
+// System Designer - Copyright 2022 Erwan Carriou
 // Licensed under the Apache License, Version 2.0 (the "License")
 runtime.install();
